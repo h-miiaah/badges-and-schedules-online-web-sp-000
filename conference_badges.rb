e72d4e2178a9f30103877a8eb763d91813a3df85
Haim D <haim11293@aol.com>
2019-09-10 17:31:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,6 @@
 # Write your code here.
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(speakers)
+  speakers.each{|speaker| puts "Hello, my name is #{speaker}."}
+end
