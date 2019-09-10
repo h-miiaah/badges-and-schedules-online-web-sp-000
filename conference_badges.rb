437e52a8b0b5c944cb7fc344a78c6683ee13675d
Haim D <haim11293@aol.com>
2019-09-10 17:37:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,11 +2,11 @@
 speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def badge_maker(name)
-  return "Hello, my name is #{name}."
+  "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(speakers)
   speakers.map do |speaker|
-    return "Hello, my name is #{speaker}."
+    "Hello, my name is #{speaker}."
   end
 end
